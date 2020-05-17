55ec33c0f4dac27e6d508c00bf98396ab278256b
Wei-Da Pan <wei-dapan@wei-das-mbp.lan>
2020-05-16 21:31:10 -0500

    Updates contactsapi hook with the correct route

--- src/hooks/useContactsApi.js
+++ src/hooks/useContactsApi.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
+import axios from 'axios';
 import { getCookie } from '../util/auth';
-import addContactApi from '../api/contacts';
 
 const useContactsApi = () => {
   const [data, setData] = useState();
@@ -18,7 +18,9 @@ const useContactsApi = () => {
           token: getCookie('userToken'),
           userName: getCookie('userName'),
         };
-        const response = await addContactApi(contactsPayload);
+        console.log(contactsPayload);
+        const response = await axios.post('/contacts/getContacts', contactsPayload);
+        console.log(response.data.data.contactList);
         setData(response.data.data.contactList);
       } catch (contactsError) {
         setError(true);
